66d9f80461ed801cabddd63234052575dd6e378e
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-10-28 10:25:33 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     proxmox = {
       source  = "Telmate/proxmox"
-      version = "2.9.11"  # Ensure compatibility with your setup
+      version = "3.0.1-rc4"  # Ensure compatibility with your setup
     }
   }
 }
