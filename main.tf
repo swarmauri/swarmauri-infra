73581093e7be186e60e4efafacba11b1d313ca22
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-10-28 07:11:05 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "proxmox" {
   pm_api_url    = var.proxmox_api_url
-  pm_user       = var.proxmox_user
-  pm_password   = var.proxmox_password
+  pm_api_token_id = var.proxmox_user
+  pm_api_token_secret = var.proxmox_password
   pm_tls_insecure = false
   pm_debug = true
 }
