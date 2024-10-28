92dd91134d4a32b0a5bac610f0a5b7f11264b160
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-10-28 07:09:57 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ provider "proxmox" {
   pm_user       = var.proxmox_user
   pm_password   = var.proxmox_password
   pm_tls_insecure = false
+  pm_debug = true
 }
 
 # Null Resource to Download the Ubuntu ISO
