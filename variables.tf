d36e70cd028d082a6fbd0b8b6f7c75ee969f04b6
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-10-30 14:44:06 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,70 +1,60 @@
-variable "proxmox_api_url" {
-  description = "The API URL for Proxmox."
-  type        = string
+variable "proxmox_ip" {
+  description = "Proxmox server IP address"
 }
 
 variable "proxmox_user" {
-  description = "The username for Proxmox."
-  type        = string
+  description = "Proxmox user with SSH access"
 }
 
-variable "proxmox_password" {
-  description = "The password for Proxmox."
-  type        = string
-  sensitive   = true  # Keeps the password hidden in outputs
+variable "proxmox_private_key" {
+  description = "SSH private key for accessing Proxmox"
 }
 
-variable "proxmox_node" {
-  description = "The name of the Proxmox node."
-  type        = string
+variable "iso_url" {
+  default     = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+  description = "URL of the Ubuntu cloud image ISO"
+}
+
+variable "iso_path" {
+  default     = "/mnt/pve/local-back/template/iso/jammy-server-cloudimg-amd64.qcow2"
+  description = "Path where the ISO will be stored on the Proxmox server"
+}
+
+variable "template_id" {
+  default     = 9001
+  description = "ID for the template VM"
 }
 
 variable "vm_count" {
-  description = "The number of VMs to create."
-  type        = number
+  default     = 3
+  description = "Number of VMs to create from the template"
 }
 
-variable "cores" {
-  description = "Number of CPU cores for each VM."
-  type        = number
+variable "vm_ids" {
+  type        = list(number)
+  default     = [100, 101, 102]
+  description = "List of unique VM IDs for each VM clone"
 }
 
-variable "memory" {
-  description = "Memory in MB for each VM."
-  type        = number
-}
-
-variable "sockets" {
-  description = "Number of sockets for each VM."
-  type        = number
-}
-
-variable "disk_sizes" {
-  description = "List of disk sizes for each VM, with units."
+variable "vm_names" {
   type        = list(string)
-  default     = ["50G", "100G"]
+  default     = ["vm-clone-1", "vm-clone-2", "vm-clone-3"]
+  description = "List of VM names for each clone"
 }
 
-variable "cloud_user" {
-  description = "The username for the cloud-init user."
-  type        = string
+variable "ips" {
+  type        = list(string)
+  default     = ["172.81.41.141/28", "172.81.41.142/28", "172.81.41.143/28"]
+  description = "List of unique IP addresses for each VM"
 }
 
-variable "cloud_password" {
-  description = "Password for the cloud-init user."
-  type        = string
-  sensitive   = true  # Keeps the password hidden in outputs
+variable "gateways" {
+  type        = list(string)
+  default     = ["172.81.41.129", "172.81.41.129", "172.81.41.129"]
+  description = "List of gateways for each VM"
 }
 
-
-variable "ssh_keys" {
-  description = "SSH public key for cloud-init user."
-  type        = string
+variable "ssh_public_key_path" {
+  default     = "/mnt/local-back/private/cobycloud.pub"
+  description = "Path to the SSH public key"
 }
-
-variable "ubuntu_iso" {
-  description = "The link to the Ubuntu ISO."
-  type        = string
-  default     = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
-}
-
